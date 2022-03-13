7028b364efcb0d18410e3c013924bb2e297387fa
safdar-azeem <safdarazeem00@gmail.com>
2022-03-13 14:55:00 +0000

    Add data.ts

---
+++ src/db/data.ts
@@ -0,0 +1,93 @@
+import song1 from "./songs/Arabesque.mp3";
+import song2 from "./songs/ClairDeLune.mp3";
+import song3 from "./songs/Reverie.mp3";
+import song4 from "./songs/TheGirlWithTheFlaxenHair.mp3";
+
+export const MusicData = [
+  {
+    id: 1,
+    title: "Virul Hits",
+    audio: song4,
+    img: "/action.jpeg",
+    type: "album",
+  },
+  {
+    id: 2,
+
+    title: "lofi-beats",
+    audio: song3,
+    img: "/lofi-beats.jpeg",
+    type: "album",
+  },
+  {
+    id: 3,
+    title: "Beast Mode",
+    audio: song4,
+    img: "/gym.jpeg",
+    type: "album",
+  },
+
+  {
+    id: 6,
+    title: "Paul Mcartney",
+    audio: song1,
+    img: "https://www.esdesignbarcelona.com/sites/default/files/img/beat_6.jpg",
+    type: "artist",
+  },
+  {
+    id: 7,
+    audio: song2,
+    title: "motivation",
+    img: "/motivation.jpeg",
+    type: "artist",
+  },
+  {
+    id: 8,
+    title: "The Arctic",
+    audio: song4,
+    img: "/arctic.webp",
+    type: "artist",
+  },
+  {
+    id: 4,
+    title: "Songs in the Car",
+    audio: song2,
+    img: "/SongstoSingintheCar.jpeg",
+    type: "album",
+  },
+  {
+    id: 5,
+    title: "Romance",
+    audio: song3,
+    img: "/romance.png",
+    type: "artist",
+  },
+  {
+    id: 9,
+    title: "The Beatles",
+    audio: song1,
+    img: "https://www.esdesignbarcelona.com/sites/default/files/img/beat_5.jpg",
+    type: "song",
+  },
+  {
+    id: 10,
+    title: "happier",
+    audio: song2,
+    img: "/happier.jpeg",
+    type: "song",
+  },
+  {
+    id: 11,
+    title: "Beatles",
+    audio: song3,
+    img: "https://www.esdesignbarcelona.com/sites/default/files/img/beat_1.jpg",
+    type: "song",
+  },
+  {
+    id: 12,
+    title: "The Beatles",
+    audio: song4,
+    img: "https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2017/10/06032547/foto-alternativa-beatles-abbey-road-subastada.jpg",
+    type: "song",
+  },
+];
