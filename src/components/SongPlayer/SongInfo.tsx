02b9a7e76aa6cfbc148f93c6bbe48172f4792e88
safdar-azeem <safdarazeem00@gmail.com>
2022-03-22 05:55:00 +0000

    Add SongInfo.tsx

---
+++ src/components/SongPlayer/SongInfo.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Item } from "../../../types";
+
+interface SongInfoProps {
+  currentMusic: Item | undefined;
+}
+
+const SongInfo: React.FC<SongInfoProps> = ({ currentMusic }) => {
+  return (
+    <div className="flex items-center gap-x-2 w-[250px]">
+      <img
+        width={60}
+        height={50}
+        src={currentMusic?.img}
+        alt={currentMusic?.title}
+      />
+      <div className="hidden md:block text-sm mr-2">
+        <p>{currentMusic?.title}</p>
+        <p className="text-gray-700">{currentMusic?.type}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SongInfo;
